5102308d5a84c200c75760351b3171636ff23a0f
TKartist <hunr2001@gmail.com>
2025-01-27 14:52:46 +0100

    initial commit of Sensitive Passive Aggresive Mailman (SPAM)

---
+++ README.md
@@ -0,0 +1,31 @@
+# SPAM (Sensitive Passive Aggresive Mailman)
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](LICENSE)
+[![Python](https://img.shields.io/badge/python-3.12-blue.svg)](https://www.python.org/)
+
+## Overview
+
+A brief description of the project, its purpose, and what it does. Highlight key features and goals.
+
+---
+
+## Table of Contents
+
+- [Overview](#overview)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [License](#license)
+- [Contact](#contact)
+
+---
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/TKartist/IM_CLAIM_SORTER.git
+
+## Usage
+
+1. Automatically collects claim emails received on a particular account and sorts/handles them accordingly
---
+++ extractor.py
@@ -0,0 +1,105 @@
+import requests as req
+from msal import ConfidentialClientApplication
+from flask import Flask, request, redirect, session, url_for
+
+import pytz
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+
+def read_secrets():
+    try:
+        with open("secret_env.txt", "r") as f:
+            credentials = f.read().splitlines()
+            dict = {}
+            for cred in credentials:
+                dict[cred.split("=")[0]] = cred.split("=")[1]
+            return dict
+    except FileNotFoundError:
+        print("File not found")
+
+
+def categorize_email(emails):
+    return "these nuts"
+
+
+@app.route('/')
+def index():
+    auth_url = msal_app.get_authorization_request_url(
+        scopes=["User.Read", "Mail.Read"],
+        redirect_uri=redirect_uri
+    )
+    return redirect(auth_url)
+
+
+@app.route('/getToken')
+def get_a_token():
+    code = request.args.get('code')
+    if not code:
+        return "Error: No authorization code provided."
+    
+    result = msal_app.acquire_token_by_authorization_code(
+        code,
+        scopes=["User.Read", "Mail.Read"],
+        redirect_uri=redirect_uri
+    )
+    if "access_token" in result:
+        session['access_token'] = result['access_token']
+        return redirect(url_for('read_emails'))
+    else:
+        return f"Error: {result.get('error')}. Description: {result.get('error_description')}"
+
+
+@app.route('/read_emails')
+def read_emails():
+    access_token = session.get('access_token')
+    if not access_token:
+        return redirect(url_for('index'))
+    url = "https://graph.microsoft.com/v1.0/me/messages?$top=50"
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    response = req.get(url, headers=headers)
+    counter = 1
+    if response.status_code == 200:
+        date_format = "%Y-%m-%dT%H:%M:%SZ"
+        emails = response.json()
+        email_list = []
+        email_parent_folders = []
+        cest = pytz.timezone("Europe/Berlin")
+        threshold_time = datetime.now(cest) - timedelta(days=1)
+
+        for email in response.json()["value"]:
+            received_time = datetime.strptime(email["receivedDateTime"], date_format).replace(tzinfo=pytz.utc).astimezone(cest)
+            if received_time > threshold_time:
+                break
+
+
+            email_dict = {
+                "subject": email.get("subject").replace('/', '_'),
+                "from" : email.get('from', {}).get('emailAddress', {}).get('address'),
+                "body": email.get("bodyPreview"),
+            }
+
+            email_list.append(email_dict)
+
+
+    else:
+        print(f"Error: {response.status_code}")
+
+
+if __name__ == "__main__":
+    creds = read_secrets()
+    redirect_uri = 'http://localhost:5000/getToken'
+
+    msal_app = ConfidentialClientApplication(
+        creds["client_id"], 
+        authority=f"{creds["authority"]}{creds["tenant"]}",
+        client_credential=creds["client"],
+    )
+
+    app.run(port=5000)
+    print("\n")
