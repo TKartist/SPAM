fcbcb47966b64c64548286f16c7caf66b719fe20
TKartist <hunr2001@gmail.com>
2025-01-31 11:00:07 +0100

    first stable version completed

--- extractor.py
+++ extractor.py
@@ -47,57 +47,63 @@ def get_emails(access_token, user_id):
     messages_endpoint = f"{GRAPH_API_URL}/users/{user_id}/messages?top=999" # top=50 means we are fetching the top 50 emails, it is capped at 999
 
     # Fetching emails at the endpoint iteratively until no more emails are left (999 at a time to avoid rate limiting)
+    res = {}
+
     while messages_endpoint:
         response = requests.get(messages_endpoint, headers=headers)
         if response.status_code == 200:
             messages = response.json().get("value", [])
-            res = {}
             print(len(messages))
 
             # Handling the emails one by one
-            for message in messages:
-
-                # filtering out automated emails, unsolicited requests, and other irrelevant emails
-                if (len(message['categories']) == 0 
-                or message['categories'] == ['Automated emails'] 
-                or message['categories'] == ['Unsolicited requests/offers']
-                or "Automatic reply" in message['subject']
-                or message['from']['emailAddress']['address'] == "rrms@surgeifrc.org"
-                or message['from']['emailAddress']['address'] == "go@ifrc.org"
-                or message['from']['emailAddress']['address'] == "GO.Staging@ifrc.org"
-                or message['from']['emailAddress']['address'] == "lars.tangen@ifrc.org"):
+            for i in range(len(messages)):
+                try:
+                    # filtering out automated emails, unsolicited requests, and other irrelevant emails
+                    message = messages[i]
+                    if (message == None 
+                    or len(message['categories']) == 0 
+                    or message['categories'] == ['Automated emails'] 
+                    or message['categories'] == ['Unsolicited requests/offers']
+                    or "Automatic reply" in message['subject']
+                    or message['from']['emailAddress']['address'] == "rrms@surgeifrc.org"
+                    or message['from']['emailAddress']['address'] == "go@ifrc.org"
+                    or message['from']['emailAddress']['address'] == "GO.Staging@ifrc.org"
+                    or message['from']['emailAddress']['address'] == "lars.tangen@ifrc.org"):
+                        continue
+                    # grouping emails by conversation ID through a dictionary
+                    if message["conversationId"] in res:
+                        res[message["conversationId"]].append({
+                            "subject": message["subject"],
+                            "from": message["from"]["emailAddress"]["address"],
+                            "receivedDateTime": message["receivedDateTime"],
+                            "body": skim_email(message["body"]["content"]),
+                            "conversationID": message["conversationId"]
+                        })
+                    else:
+                        res[message["conversationId"]] = [{
+                            "subject": message["subject"],
+                            "from": message["from"]["emailAddress"]["address"],
+                            "receivedDateTime": message["receivedDateTime"],
+                            "body": skim_email(message["body"]["content"]),
+                            "conversationID": message["conversationId"]
+                        }]
+                except Exception as e:
+                    print("Error:", e)
                     continue
-
-                # grouping emails by conversation ID through a dictionary
-                if message["conversationId"] in res:
-                    res[message["conversationId"]].append({
-                        "subject": message["subject"],
-                        "from": message["from"]["emailAddress"]["address"],
-                        "receivedDateTime": message["receivedDateTime"],
-                        "body": skim_email(message["body"]["content"]),
-                        "conversationID": message["conversationId"]
-                    })
-                else:
-                    res[message["conversationId"]] = [{
-                        "subject": message["subject"],
-                        "from": message["from"]["emailAddress"]["address"],
-                        "receivedDateTime": message["receivedDateTime"],
-                        "body": skim_email(message["body"]["content"]),
-                        "conversationID": message["conversationId"]
-                    }]
-        # store the filtered emails as a text file
-        # to be updated to directly store in a database and perform sentiment analysis in the future
-        # with open("output.txt", "w") as f:
-        #     for _, val in res.items():
-        #         if len(val) > 1:
-        #             f.write(str(val) + "\n")
-        # f.close()
+        
         else:
             raise Exception(f"Error fetching emails: {response.status_code} {response.text}")
-        messages_endpoint = response.json().get("@odata.nextLink")
+        messages_endpoint = response.json().get("@odata.nextLink", None)
         if not messages_endpoint:
             print("Reached the last page of messages.")
             break  # Stop if no more pages exist
+    # store the filtered emails as a text file
+        # to be updated to directly store in a database and perform sentiment analysis in the future
+    with open("output.txt", "w") as f:
+        for _, val in res.items():
+            if len(val) > 1:
+                f.write(str(val) + "\n")
+        f.close()
 
 
 # Count total number of emails just in case there is a mismatch
@@ -127,10 +133,11 @@ if __name__ == "__main__":
         token = get_access_token(credentials)
         print("Access token acquired successfully.")
         user_id = "im@ifrc.org"
-        total_emails = email_count(token, user_id)
+        # total_emails = email_count(token, user_id)
 
         # store all read emails in a text file (for now)
-        # get_emails(token, user_id, total_emails)
+        get_emails(token, user_id)
     except Exception as e:
+        print("shinbal")
         print(f"Error: {e}")
 
