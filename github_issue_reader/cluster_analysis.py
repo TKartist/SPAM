ec8ddc288cf7ade7075a2ce2214f0248c676f14e
TKartist <hunr2001@gmail.com>
2025-04-07 11:17:18 +0200

    changing user feedback data struct

--- github_issue_reader/cluster_analysis.py
+++ github_issue_reader/cluster_analysis.py
@@ -7,9 +7,11 @@ import string
 
 def understanding_clusters():
     df = pd.read_csv("../BERT_TOPIC_CLUSTER.csv")
+    df = df[["issue", "topic", "issue_link"]]
+    df.to_csv("../safer_cluster.csv", index=False)
     dfs_by_topic = {topic: group_df for topic, group_df in df.groupby("topic")}
     for topic, group_df in dfs_by_topic.items():
-        group_df = group_df[["issue", "topic"]]
+        group_df = group_df[["issue", "topic", "issue_link"]]
         group_df.to_excel(f"../topic_split/topic_{topic+1}.xlsx", index=False)
         
 
